5585e7f2e6f792b5ec0cc218dcce963a8c868f5b
Muhammad Salah <muhammadsalah217@gmail.com>
2024-02-28 06:57:17 +0200

    added a delete all button to reset leads

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 let myLeads = [];
 
 const saveInputBtn = document.getElementById("input-btn");
+const deleteInputBtn = document.getElementById("delete-btn");
 let inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
-
-saveInputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-  console.log(localStorage.getItem("myLeads"));
-});
 
 function renderLeads() {
   let listItems = "";
@@ -35,3 +27,18 @@ function renderLeads() {
 
   ulEl.innerHTML = listItems;
 }
+
+deleteInputBtn.addEventListener("dblclick", () => {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
+
+saveInputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+  console.log(localStorage.getItem("myLeads"));
+});

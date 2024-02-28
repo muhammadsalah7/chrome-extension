1961effd057fd42df0d5a91e6f3a8489582fb762
Muhammad Salah <muhammadsalah217@gmail.com>
2024-02-28 06:31:26 +0200

    saving input in local storage

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let myLeads = [];
+
 const saveInputBtn = document.getElementById("input-btn");
 let inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
 
-saveInputBtn.addEventListener("click", () => {
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+saveInputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
 
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
@@ -25,4 +35,3 @@ function renderLeads() {
 
   ulEl.innerHTML = listItems;
 }
-//https://www.google.com
